ffeb0cb0c390ca96be2afbf90533caa337b7b7a7
ihuangGitHub <64209339+ihuangGitHub@users.noreply.github.com>
2020-04-24 12:39:31 -0400

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,7 +16,7 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
-            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e =>e.AverageGrade.ToList())
+            var grades = Students.OrderByDescending(e => e.averageGrade).Select(e => e.averageGrade.ToList());
             if (grades[threshold - 1] <= averageGrade)
                 return 'A';
             else if (grades[(threshold * 2) - 1] <= averageGrade)
