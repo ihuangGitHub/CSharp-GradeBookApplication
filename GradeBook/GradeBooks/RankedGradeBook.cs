06b5ebf4663544e9ebee94f444bb0ed68884f7f3
ihuangGitHub <64209339+ihuangGitHub@users.noreply.github.com>
2020-04-24 12:55:52 -0400

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -25,7 +25,7 @@ namespace GradeBook.GradeBooks
                 return 'C';
             else if (grades[(threshold * 4) - 1] <= averageGrade)
                 return 'D';
-            else return 'F'; */
+            else*/ return 'F'; 
         }
 
     }
